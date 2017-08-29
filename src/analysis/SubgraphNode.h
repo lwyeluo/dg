618caf8542b46a16cbf00acb460dc22f78fe4f39
Marek Chalupa <mchqwerty@gmail.com>
2017-08-29 14:35:34 +0200

    SubgraphNode: add users edges
    allow access nodes that use given node
    
    Signed-off-by: Marek Chalupa <mchqwerty@gmail.com>

--- src/analysis/SubgraphNode.h
+++ src/analysis/SubgraphNode.h
@@ -30,6 +30,8 @@ protected:
     std::vector<NodeT *> predecessors;
     // XXX: maybe we could use SmallPtrVector or something like that
     std::vector<NodeT *> operands;
+    // nodes that use this node
+    std::vector<NodeT *> users;
 
     // size of the memory
     size_t size;
@@ -95,6 +97,10 @@ public:
         return operands[idx];
     }
 
+    const std::vector<NodeT *>& getUsers() const {
+        return users;
+    }
+
     size_t getOperandsNum() const
     {
         return operands.size();
@@ -102,8 +108,14 @@ public:
 
     size_t addOperand(NodeT *n)
     {
+#ifdef DEBUG_ENABLED
+        for (NodeT *x : operands)
+            assert(x != n && "Operand added multiple times");
+#endif
         assert(n && "Passed nullptr as the operand");
         operands.push_back(n);
+        n->addUser(static_cast<NodeT *>(this));
+
         return operands.size();
     }
 
@@ -245,6 +257,18 @@ public:
     {
         return successors.size();
     }
+
+private:
+    void addUser(NodeT *nd) {
+#ifdef DEBUG_ENABLED
+        for (NodeT *x : users)
+            assert(x != nd && "User added multiple times");
+#endif // DEBUG_ENABLED
+
+        users.push_back(nd);
+    }
+
+
 };
 
 } // analysis
